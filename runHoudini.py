696d8c1f41f63273e3dbafb0a7f82340f0d2fe4a
eshatley <49348401+eshatley@users.noreply.github.com>
2020-08-12 17:50:48 -0500

    Update runHoudini.py
    Added code for environment setup

--- runHoudini.py
+++ runHoudini.py
@@ -1,10 +1,19 @@
 #A wrapper that will run Houdini in a custom environment
 
-import subrocess
+import os, subrocess
 
 #Set the path to Houdini's executable file. Note to self, double check Houdini version
 #Once we have the path, Python can open it.
 houdini = 'C:/Program Files/Side Effects Software/Houdini 16.5.536/bin/houdinifx.exe'
+
+#Setup environment
+#Get path to folder where pipeline tools are located
+rootPipeline = os.path.dirname(__file__).replace('\\','/')
+os.environ['PYTHONPATH'] = '{}/tools;&'.format(rootPipeline)
+#Variable pointing at pipeline tools folder
+os.environ['ROOT_PIPELINE'] = rootPipeline
+#Set project root
+os.environ['JOB'] = '{}/3D'.format(os.path.dirname(os.path.dirname(__file__)))
 
 #Open up Houdini
 subprocess.Popen(houdini)
